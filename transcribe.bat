8736a7ca3d6d1bc67187e741121e05b66bf389d0
Ahrimdon <mrcodblackops10@gmail.com>
2023-08-07 01:22:34 -0400

    Fixed error

--- transcribe.bat
+++ transcribe.bat
@@ -3,9 +3,9 @@
 set FULLNAME="Voice Training.wav"
 
 :: Remove extension from filename
-for %%A in ("%FULLNAME%") do set "FILENAME=%%~nA"
+for %%A in (%FULLNAME%) do set "FILENAME=%%~nA"
 
 :: Create the output directory if it does not exist
 if not exist "output/%FILENAME%" mkdir "output/%FILENAME%"
 
-whisper "input/%FULLNAME%" --model small.en --language en --fp16 False --task transcribe --output_dir "output/%FILENAME%"
+whisper input/%FULLNAME% --model tiny.en --language en --fp16 False --task transcribe --output_dir "output/%FILENAME%"
