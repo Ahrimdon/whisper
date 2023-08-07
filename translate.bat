69ae97ac3b32a43fa61f1624c25765f3258ef1dc
Ahrimdon <mrcodblackops10@gmail.com>
2023-08-07 01:50:47 -0400

    Updated transcribe.bat and added translate.bat

--- translate.bat
+++ translate.bat
@@ -1,16 +1,16 @@
 @echo on
 
-set FULLNAME="Transcribe.wav"
-set LANGUAGE=en
-set model=small
+set FULLNAME="Translate.wav"
+set LANGUAGE=es
+set model=large
 
 :: Remove extension from filename
 for %%A in (%FULLNAME%) do set "FILENAME=%%~nA"
 
 :: Create the output directory if it does not exist
-if not exist "output/transcriptions/%FILENAME%" mkdir "output/transcriptions/%FILENAME%"
+if not exist "output/translations/%FILENAME%" mkdir "output/translations/%FILENAME%"
 
-whisper input/%FULLNAME% --model %model% --language %LANGUAGE% --fp16 False --task transcribe --output_dir "output/transcriptions/%FILENAME%"
+whisper input/%FULLNAME% --model %model% --language %LANGUAGE% --fp16 False --task translate --output_dir "output/translations/%FILENAME%"
 
 :: Languages - {af,am,ar,as,az,ba,be,bg,bn,bo,br,bs,ca,cs,cy,da,de,el,en,es,et,eu,fa,fi,fo,fr,gl,gu,ha,haw,he,hi,hr,ht,hu,hy,id,is,it,ja,jw,ka,kk,km,kn,ko,la,lb,ln,lo,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,my,ne,nl,nn,no,oc,pa,pl,ps,pt,ro,ru,sa,sd,si,sk,sl,sn,so,sq,sr,su,sv,sw,ta,te,tg,th,tk,tl,tr,tt,uk,ur,uz,vi,yi,yo,zh,Afrikaans,Albanian,Amharic,Arabic,Armenian,Assamese,Azerbaijani,Bashkir,Basque,Belarusian,Bengali,Bosnian,Breton,Bulgarian,Burmese,Castilian,Catalan,Chinese,Croatian,Czech,Danish,Dutch,English,Estonian,Faroese,Finnish,Flemish,French,Galician,Georgian,German,Greek,Gujarati,Haitian,Haitian Creole,Hausa,Hawaiian,Hebrew,Hindi,Hungarian,Icelandic,Indonesian,Italian,Japanese,Javanese,Kannada,Kazakh,Khmer,Korean,Lao,Latin,Latvian,Letzeburgesch,Lingala,Lithuanian,Luxembourgish,Macedonian,Malagasy,Malay,Malayalam,Maltese,Maori,Marathi,Moldavian,Moldovan,Mongolian,Myanmar,Nepali,Norwegian,Nynorsk,Occitan,Panjabi,Pashto,Persian,Polish,Portuguese,Punjabi,Pushto,Romanian,Russian,Sanskrit,Serbian,Shona,Sindhi,Sinhala,Sinhalese,Slovak,Slovenian,Somali,Spanish,Sundanese,Swahili,Swedish,Tagalog,Tajik,Tamil,Tatar,Telugu,Thai,Tibetan,Turkish,Turkmen,Ukrainian,Urdu,Uzbek,Valencian,Vietnamese,Welsh,Yiddish,Yoruba}
 
