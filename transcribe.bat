1ecead9fbfad9957175b76053cf73f7f7c3aa0d2
Ahrimdon <mrcodblackops10@gmail.com>
2023-08-07 01:26:07 -0400

    Changed default model

--- transcribe.bat
+++ transcribe.bat
@@ -8,4 +8,4 @@ for %%A in (%FULLNAME%) do set "FILENAME=%%~nA"
 :: Create the output directory if it does not exist
 if not exist "output/%FILENAME%" mkdir "output/%FILENAME%"
 
-whisper input/%FULLNAME% --model tiny.en --language en --fp16 False --task transcribe --output_dir "output/%FILENAME%"
+whisper input/%FULLNAME% --model small --language en --fp16 False --task transcribe --output_dir "output/%FILENAME%"
